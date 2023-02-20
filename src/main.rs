5d808b706cd5590fc0fcad90bf1f0d8bc54d83ca
pc <johnathan.stark42@gmail.com>
2023-02-20 19:36:43 -0300

    create app

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use std::error::Error;
 use rocket::routes;
 use tokio;
 
+//mongo  stw  l1sLXHUz01OACdof
+
+/*
+
+use mongodb::{bson::doc, options::ClientOptions, Client};
+#[tokio::main]
+    async fn main() -> mongodb::error::Result<()> {
+        let client_options = ClientOptions::parse(
+            "mongodb+srv://stw:<password>@chat-wp.pmlgafg.mongodb.net/?retryWrites=true&w=majority",
+        )
+        .await?;
+        let client = Client::with_options(client_options)?;
+        let database = client.database("testDB");
+        Ok(())
+    }
+
+
+*/
+
+
 
 mod model;
 pub mod schema;
