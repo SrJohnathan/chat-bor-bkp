7a0dcb9767658fe9680d120b41a9f23b52da9702
pc <johnathan.stark42@gmail.com>
2023-03-07 10:44:06 -0300

    create app

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,7 @@ async fn main() {
                     new_job_sp.app.as_str(),
                     new_job_sp.number.as_str(), get_number_app(new_job_sp.app.as_str()),
                     serde_json::to_value(
-                        MessageText { type_field: "text".to_string(), text: "escolha uma opção por favor.".to_string() }
+                        MessageText { type_field: "text".to_string(), text: "Escolha uma opção por favor.".to_string() }
                     ).unwrap());
 
                 let mut vec = Vec::new();
