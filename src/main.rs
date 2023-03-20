31d4d538113126f8a35b00378aaac9dffb24dbd9
pc <johnathan.stark42@gmail.com>
2023-03-20 15:19:11 -0300

    create app

--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,7 @@ async fn main() {
                                http::insta_controller::messaging_webhook
 
                        ])
-                      .mount("/public", FileServer::from(rocket::fs::relative!("static")))
+                   //   .mount("/public", FileServer::from(rocket::fs::relative!("static")))
                     .launch()
                     .await;
             }
