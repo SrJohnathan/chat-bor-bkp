27bfc9702b7a23a459ee5e1b25b53cf79a1d24ee
Johnathan <johnathan.stark42@gmail.com>
2023-07-29 16:19:33 -0300

    m

--- src/http/gupshup_controller.rs
+++ src/http/gupshup_controller.rs
@@ -1,31 +1,31 @@
 use std::future::Future;
-use reqwest::Client;
+use reqwest::{Client, Error, Response};
 use rocket::http::Status;
 use rocket::response::status::{BadRequest, Created};
 use rocket::serde::json::Json;
 use rocket::{post, State};
+use serde_json::to_string;
 use tokio::sync::mpsc::Sender;
-use crate::chat::{ ChatWP};
+use crate::chat::{ChatWP};
 use crate::chat::db_mongo::MongoDb;
-use crate::cofg::NewJob;
+use crate::chat::send_list_wp::{MessageText, SendWP};
+use crate::cofg::{get_number_app, NewJob};
 use crate::http::models::{Audio, ButtonReply, Delivered, Enqueued, Failed, File, Image, ListReply, Location, MessageEvent, MessageGP, ParentMessage, Read, Sent, Text, Video};
-use crate::model::mongo::{ select_status};
+use crate::model::mongo::{select_status};
 
 #[post("/whatsapp/chatbot", format = "application/json", data = "<task>")]
-pub async fn web_hook(db:MongoDb<'_>, job:&State<Sender<String>>,task: Json<serde_json::Value>)
-    -> Status {
-
+pub async fn web_hook(db: MongoDb<'_>, job: &State<Sender<String>>, task: Json<serde_json::Value>)
+                      -> Status {
     let message = task.0;
     let d = message.get("type");
     let req: Client = Client::new();
-
 
 
     match d {
         None => { println!("não encontrou type") }
         Some(c) => {
 
-           // println!("{:?}",message);
+            // println!("{:?}",message);
 
             let app = message.get("app").unwrap();
 
@@ -35,14 +35,12 @@ pub async fn web_hook(db:MongoDb<'_>, job:&State<Sender<String>>,task: Json<serd
                 let ty = pl.get("type").unwrap();
 
 
-
                 if ty.as_str().unwrap().eq(&"enqueued".to_string()) {
                     let msg: ParentMessage<MessageEvent<Enqueued>> = serde_json::from_str(&message.to_string()).unwrap();
                 } else if ty.as_str().unwrap().eq(&"failed".to_string()) {
                     let msg: ParentMessage<MessageEvent<Failed>> = serde_json::from_str(&message.to_string()).unwrap();
 
-                    println!("{:?}",msg);
-
+                    println!("{:?}", msg);
                 } else if ty.as_str().unwrap().eq(&"sent".to_string()) {
                     let msg: ParentMessage<MessageEvent<Sent>> = serde_json::from_str(&message.to_string()).unwrap();
                 } else if ty.as_str().unwrap().eq(&"delivered".to_string()) {
@@ -51,47 +49,60 @@ pub async fn web_hook(db:MongoDb<'_>, job:&State<Sender<String>>,task: Json<serd
                     let msg: ParentMessage<MessageEvent<Read>> = serde_json::from_str(&message.to_string()).unwrap();
                 } else {}
             } else if c.as_str().unwrap().eq("message") {
-
                 let pl = message.get("payload").unwrap();
                 let ty = pl.get("type").unwrap();
                 let number = pl.get("source").unwrap();
-                let mut chat = ChatWP::new(number.as_str().unwrap(),app.as_str().unwrap());
-
-
-
-
+                let mut chat = ChatWP::new(number.as_str().unwrap(), app.as_str().unwrap());
 
 
                 if ty.as_str().unwrap().eq(&"text".to_string()) {
-
                     let msg: ParentMessage<MessageGP<Text>> = serde_json::from_str(&message.to_string()).unwrap();
 
 
-                    req.post("https://siga-telecom.herokuapp.com/api/v1/whatsapp/webHookSocket")
+                    let name = req.post("https://siga-telecom.herokuapp.com/api/v1/whatsapp/webHookSocket")
                         // .header("Content-Type", "application/json")
                         .json(&msg)
                         .send().await;
 
 
-                    chat.add_props(String::from("nodedouser"),msg.payload.sender.name);
+                    match name {
+                        Ok(x) => {
 
-                    match  chat.run(&db).await {
-                        Ok(c) => {
-                            let e = NewJob{
-                                number: c.number.clone(),
-                                etapa: c.st.clone(),
-                                time: 0,
-                                app: c.app.clone()
-                            };
+                            let tex = x.text().await.unwrap();
 
-                            match   job.send(serde_json::to_string(&e).unwrap()).await {
-                                Ok(x) => {}
-                                Err(e) => { println!("{}",e.0) }
+                            if tex != "null".to_string() {
+
+                                chat.add_props(String::from("nodedouser"), tex);
+
+                            } else {
+                                chat.add_props(String::from("nodedouser"), msg.payload.sender.name);
                             }
+
                         }
-                        Err(e) => { println!("erro {}",e)}
+                        Err(e) => {
+                            chat.add_props(String::from("nodedouser"), msg.payload.sender.name);
+                        }
                     }
 
+                    if is_bot(&req, msg.app.clone(), msg.payload.sender.phone).await {
+
+                        match chat.run(&db).await {
+                            Ok(c) => {
+                                let e = NewJob {
+                                    number: c.number.clone(),
+                                    etapa: c.st.clone(),
+                                    time: 0,
+                                    app: c.app.clone(),
+                                };
+
+                                match job.send(serde_json::to_string(&e).unwrap()).await {
+                                    Ok(x) => {}
+                                    Err(e) => { println!("{}", e.0) }
+                                }
+                            }
+                            Err(e) => { println!("erro {}", e) }
+                        }
+                    }
                 } else if ty.as_str().unwrap().eq(&"image".to_string()) {
                     let msg: ParentMessage<MessageGP<Image>> = serde_json::from_str(&message.to_string()).unwrap();
                 } else if ty.as_str().unwrap().eq(&"file".to_string()) {
@@ -112,39 +123,31 @@ pub async fn web_hook(db:MongoDb<'_>, job:&State<Sender<String>>,task: Json<serd
                         .send().await;
 
 
-                    match  chat.run_button(&msg.payload.payload.title,&db).await {
+                    match chat.run_button(&msg.payload.payload.title, &db).await {
                         Ok(c) => {
-
-                            let e =  if c.st.as_str() == "exit" {
-
-                                NewJob{
+                            let e = if c.st.as_str() == "exit" {
+                                NewJob {
                                     number: c.number.clone(),
                                     etapa: "exit".to_string(),
                                     time: 0,
-                                    app: c.app.clone()
+                                    app: c.app.clone(),
                                 }
-
                             } else {
-
-                                NewJob{
+                                NewJob {
                                     number: c.number.clone(),
                                     etapa: c.st.clone(),
                                     time: 0,
-                                    app: c.app.clone()
+                                    app: c.app.clone(),
                                 }
-
                             };
 
-                            match   job.send(serde_json::to_string(&e).unwrap()).await {
+                            match job.send(serde_json::to_string(&e).unwrap()).await {
                                 Ok(x) => {}
-                                Err(e) => { println!("{}",e.0) }
+                                Err(e) => { println!("{}", e.0) }
                             }
-
                         }
-                        Err(e) => { println!("erro {}",e)}
+                        Err(e) => { println!("erro {}", e) }
                     }
-
-
                 } else if ty.as_str().unwrap().eq(&"list_reply".to_string()) {
                     let msg: ParentMessage<MessageGP<ListReply>> = serde_json::from_str(&message.to_string()).unwrap();
 
@@ -154,64 +157,93 @@ pub async fn web_hook(db:MongoDb<'_>, job:&State<Sender<String>>,task: Json<serd
                         .json(&msg)
                         .send().await;
 
-                    chat.add_props(String::from("nodedouser"),msg.payload.sender.name);
+                    chat.add_props(String::from("nodedouser"), msg.payload.sender.name);
 
-                    let tmpstr = msg.payload.payload.postbackText.replace("n","");
+                    let tmpstr = msg.payload.payload.postbackText.replace("n", "");
                     let my_str = tmpstr.trim().parse::<i32>().unwrap();
 
 
-
-                    match  msg.payload.payload.title.as_str() {
-
-                        "Voltar" =>{
+                    match msg.payload.payload.title.as_str() {
+                        "Voltar" => {
                             match chat.back(&db).await {
-                                Ok(c) => {
-
-                                }
-                                Err(e) =>{}
+                                Ok(c) => {}
+                                Err(e) => {}
                             }
                         }
 
                         &_ => {
-
-
-                            match  chat.run_list(&(my_str +1).to_string(),&db).await {
+                            match chat.run_list(&(my_str + 1).to_string(), &db).await {
                                 Ok(c) => {
-
-
-                                    let e = NewJob{
+                                    let e = NewJob {
                                         number: c.number.clone(),
                                         etapa: c.st.clone(),
                                         time: 0,
-                                        app: c.app.clone()
+                                        app: c.app.clone(),
                                     };
 
-                                    match   job.send(serde_json::to_string(&e).unwrap()).await {
+                                    match job.send(serde_json::to_string(&e).unwrap()).await {
                                         Ok(x) => {}
-                                        Err(e) => { println!("{}",e.0) }
+                                        Err(e) => { println!("{}", e.0) }
                                     }
-
                                 }
-                                Err(e) => { println!("erro {}",e)}
+                                Err(e) => { println!("erro {}", e) }
                             }
                         }
-
-
                     }
-
-
-
-
                 } else {}
-
-
-
-            }else {}
+            } else {}
         }
     }
 
 
     Status::Ok
+}
 
 
+async fn is_bot(req: &reqwest::Client, app: String, number: String) -> bool {
+    let tmp = format!("https://siga-telecom.herokuapp.com/api/v1/whatsapp/isbot/{}/{}", app, number);
+
+    // Realiza a solicitação HTTP e aguarda a resposta
+    let response = req.get(&tmp).send().await;
+
+    // Verifica se houve algum erro na solicitação HTTP
+    match response {
+        Ok(resp) => {
+            // Verifica se o status da resposta é bem-sucedido (código 2xx)
+            if resp.status().is_success() {
+                // Lê o conteúdo da resposta como texto
+                let body = resp.text().await;
+
+                // Verifica se houve algum erro ao obter o conteúdo da resposta
+                match body {
+                    Ok(text) => {
+                        // Converte o texto em um bool
+                        if text.trim() == "true" {
+                            true
+                        } else if text.trim() == "false" {
+                            false
+                        } else {
+                            // Resposta inválida da API, trata o erro como preferir
+                            eprintln!("Resposta inválida da API: {}", text);
+                            false
+                        }
+                    }
+                    Err(err) => {
+                        // Tratamento de erro ao obter o corpo da resposta
+                        eprintln!("Erro ao obter o corpo da resposta: {}", err);
+                        false // ou tratar o erro de outra forma, dependendo do caso
+                    }
+                }
+            } else {
+                // Tratamento de erro para resposta com status diferente de 2xx (erro HTTP)
+                eprintln!("Erro na resposta HTTP: {:?}", resp.status());
+                false // ou tratar o erro de outra forma, dependendo do caso
+            }
+        }
+        Err(err) => {
+            // Tratamento de erro na solicitação HTTP
+            eprintln!("Erro na solicitação HTTP: {}", err);
+            false // ou tratar o erro de outra forma, dependendo do caso
+        }
+    }
 }
