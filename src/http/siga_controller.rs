ab380d3165c485bdcc19f0fabf8a46342e032e29
Johnathan <johnathan.stark42@gmail.com>
2023-08-22 17:07:28 -0300

    m

--- src/http/siga_controller.rs
+++ src/http/siga_controller.rs
@@ -188,6 +188,20 @@ pub async fn agente(task: Json<serde_json::Value>) -> Result<Created<String>, St
                     Ok(status::Created::new("".to_string()).body("".to_string()))
                 } else if ty.as_str().unwrap().eq(&"image".to_string()) {
                     let msg: ParentMessage<MessageGP<Image>> = serde_json::from_str(&message.to_string()).unwrap();
+
+
+                    tokio::spawn(async move {
+                        let response = req.post("https://siga-telecom.herokuapp.com/api/v1/whatsapp/webHookSocket")
+                            // .header("Content-Type", "application/json")
+                            .json(&msg)
+                            .send().await;
+                        match response {
+                            Ok(e) => { Ok(status::Created::new("".to_string()).body("".to_string())) }
+                            Err(s) => { Err(s.to_string()) }
+                        }
+                    });
+
+
                     Ok(status::Created::new("".to_string()).body("".to_string()))
                 } else if ty.as_str().unwrap().eq(&"file".to_string()) {
                     let msg: ParentMessage<MessageGP<File>> = serde_json::from_str(&message.to_string()).unwrap();
